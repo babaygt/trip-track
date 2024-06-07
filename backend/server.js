05ee6d78cea83e74fc1c2ebd6fadb3c13bfe9f07
babaygt <yigitingbaba@gmail.com>
2024-06-07 21:42:16 +0200

    Configure CORS settings

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+
 const PORT = process.env.PORT || 3500
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
 
+app.use('/', express.static(path.join(__dirname, '/public')))
 app.use('/', require('./routes/root'))
 
 app.all('*', (req, res) => {
