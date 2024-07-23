6076fbe8adc490f8b090498b17afc716d4a826d4
babaygt <yigitingbaba@gmail.com>
2024-07-23 15:38:41 +0200

    Add userSlice to store user state in Redux

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { baseApiSlice } from '../api/baseApiSlice'
 import authReducer from '../features/auth/authSlice'
+import userReducer from '../features/users/userSlice'
 
 export const store = configureStore({
 	reducer: {
 		[baseApiSlice.reducerPath]: baseApiSlice.reducer,
 		auth: authReducer,
+		user: userReducer,
 	},
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware().concat(baseApiSlice.middleware),
