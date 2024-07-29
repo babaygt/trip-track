f0be112bdb325482ff0a3220f784055134a981b7
babaygt <yigitingbaba@gmail.com>
2024-07-29 10:29:16 +0200

    Initialize react-modal package - Set up Modal.setAppElement in main.jsx.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
-
+import Modal from 'react-modal'
 import './index.css'
+
+Modal.setAppElement('#root')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
